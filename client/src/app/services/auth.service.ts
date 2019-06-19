54c5436b565f74bb6655151a89803d605ad477b8
Kevin <stradtkt22@gmail.com>
2019-06-19 05:16:57 -0400

    Added register

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -15,6 +15,12 @@ export class AuthService {
                 .then(userData => resolve(userData), err => reject(err));
         });
     }
+    register(email: string, password: string) {
+        return new Promise((resolve, reject) => {
+            this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+                .then(userData => resolve(userData), err => reject(err));
+        });
+    }
     getAuth() {
         return this.afAuth.authState.pipe(map(auth => auth));
     }
