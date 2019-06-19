4c2e4c82248757f35f6f418e1d584dea8f465796
Kevin <stradtkt22@gmail.com>
2019-06-19 04:31:11 -0400

    Added the logout

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Client} from '../../models/Client';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+    isLoggedIn: boolean;
+    loggedInUser: string;
+    showRegister: boolean;
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private flashMessage: FlashMessagesService
+    ) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.authService.getAuth().subscribe(auth => {
+            if(auth) {
+                this.isLoggedIn = true;
+                this.loggedInUser = auth.email;
+            } else {
+                this.isLoggedIn = false;
+            }
+        });
+    }
+    onLogoutClick() {
+        this.authService.logout();
+        this.flashMessage.show('You are now logged out', {
+            cssClass: 'alert-success', timeout: 4000
+        });
+        this.router.navigate(['/login']);
+    }
 
 }
